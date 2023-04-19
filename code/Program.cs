fcb732bac241c4d1dfb3fee0975dfff20db0ea7b
Verenich Tatiana <verenik91@gmail.com>
2023-04-19 22:10:47 +0300

    Добавлен метод cоздания нового массива с присваиванием элементов подходящих условию

--- code/Program.cs
+++ code/Program.cs
@@ -3,6 +3,19 @@
 
 
 
+void ShortStrings(string[] array, string[] creatArray, int num)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= num)
+        {
+            creatArray[j] = array[i];
+            j++;
+        }
+    }
+}
+
 int CountSizeArray(string[] array, int num)
 {
     int count = 0;
