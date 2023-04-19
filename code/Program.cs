23cc3884e904a10d6ae247e00a5f47bc7d3a27ad
Verenich Tatiana <verenik91@gmail.com>
2023-04-19 22:08:32 +0300

    Добавлен метод подсчёта количества подходящих под условие элементов для определения размера будущего массива

---
+++ code/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+
+int CountSizeArray(string[] array, int num)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= num)
+            count++;
+    }
+    return count;
+}
